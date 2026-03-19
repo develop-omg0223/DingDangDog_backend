a5cf8aaf537d16fa39d2379934061b90e914bfb3
develop-omg0223 <lalalu0321@gmail.com>
2026-03-19 09:55:44 +0900

    260319 09:55 오명근 충돌 해결 테스트

--- DingDangDog/src/main/java/com/ddd/app/mypage/dao/MypageDAO.java
+++ DingDangDog/src/main/java/com/ddd/app/mypage/dao/MypageDAO.java
@@ -1,9 +1,10 @@
 package com.ddd.app.mypage.dao;
 
-import java.util.Map;
+import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 
+import com.ddd.app.mypage.dto.MyPageDogMatchingDTO;
 import com.ddd.app.user.dto.UserDTO;
 import com.ddd.config.MyBatisConfig;
 
@@ -20,29 +21,23 @@ public class MypageDAO {
 		System.out.println("비밀번호 체크");
 		return sqlSession.selectOne("MPCInfo.checkPassword", userDTO);
 	}
-
-	// 정보변경페이지용 정보 조회
-	public UserDTO selectMyPageInfo(int UserNumber) {
-		System.out.println("마이페이지 정보 조회");
-		return sqlSession.selectOne("MPCInfo.selectMyPageInfo", UserNumber);
+	// ===== 매칭 결과 저장 =====
+	public void insertMatchingResult(MyPageDogMatchingDTO matchingDTO) {
+		sqlSession.insert("myPageMatching.insertMatchingResult", matchingDTO);
 	}
 
-	// 닉네임 체크
-	public boolean checkNickName(Map<String, Object> checkMap) {
-		System.out.println("닉네임 체크");
-		return (Integer) (sqlSession.selectOne("MPCInfo.checkNickName", checkMap)) < 1;
+	// ===== userNumber 매칭 이력 전체 조회 =====
+	public List<MyPageDogMatchingDTO> selectMatchingList(int userNumber) {
+		return sqlSession.selectList("myPageMatching.selectMatchingList", userNumber);
 	}
 
-	// 정보수정
-	public void updateMyPageInfo(Map<String, Object> updateMap) {
-		System.out.println("마이페이지 정보 수정");
-		sqlSession.update("MPCInfo.updateMyPageInfo", updateMap);
+	// ===== userNumber 현재 저장된 매칭 이력 개수 확인 =====
+	public int countSavedResults(int userNumber) {
+		return sqlSession.selectOne("myPageMatching.countSavedResults", userNumber);
 	}
 
-	// 회원 탈퇴처리
-	public void updateToWithdraw(int userNumber) {
-		System.out.println("회원 탈퇴 처리 시작 " + userNumber + "번 회원");
-		sqlSession.update("MPCInfo.updateToWithdraw", userNumber);
+	// ===== 가장 오래된 결과 삭제 =====
+	public void deleteOldestResult(int userNumber) {
+		sqlSession.delete("myPageMatching.deleteOldestResult", userNumber);
 	}
-
 }
